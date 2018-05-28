fe516425373e663de66df0eddd926f3150132861
Jesse <xinming.feng@tongdun.cn>
2018-05-28 14:40:16 +0800

    增加async函数验证

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # celia
+
+> Note:  A modern JavaScript utility library delivering modularity, performance, & extras.
+
+---
+
+<br/>
+
+# Usage
+
+```bash
+
+// const { type, common, array } = require('celia');
+const type = require('celia/lib/type');
+const common = require('celia/lib/common');
+const array = require('celia/lib/array');
+
+```
--- examples/array.js
+++ examples/array.js
@@ -3,18 +3,34 @@
 const array = require('../lib/array');
 
 const arr1 = [1, 2, 3, 4, 5];
-array.forEach(arr1, n => console.log(n));
 
 const obj1 = {
   a: 'a',
   b: 'b',
   c: 'c'
 };
-array.forEach(obj1, (val, key) => console.log(`Key:${key}, Value:${val}`));
+
+function Obj() {
+  this.a = 'a';
+  this.b = 'b';
+  this.c = 'c';
+}
+
+Obj.prototype.fn = () => {};
+
+const obj2 = new Obj();
+
+array.forEach(arr1, n => console.log(n));
+
+array.forEach(obj1, (val, key) => console.log('forEach', `Key:${key}, Value:${val}`));
+
+array.forEach(obj2, (val, key) => console.log('forEach', `Key:${key}, Value:${val}`));
 
 array.each(arr1, (i, n) => console.log(n));
 
-array.each(obj1, (key, val) => console.log(`Key:${key}, Value:${val}`));
+array.each(obj1, (key, val) => console.log('each', `Key:${key}, Value:${val}`));
+
+array.each(obj2, (key, val) => console.log('each', `Key:${key}, Value:${val}`));
 
 console.log('merge', array.merge(['a', 'b', 'c'], arr1));
 
