b3587a4853f33bd76ecf13ff2ffdfc07ec744767
奉新明 <fxm0016@gmail.com>
2019-06-17 15:01:43 +0800

    refactor(camelCase): 支持更多分割符号

--- src/camelCase.js
+++ src/camelCase.js
@@ -1,7 +1,11 @@
 import { DASH_ALPHA_REGEX } from './_internal/_regex';
+import isString from './isString';
 
 export default function (value) {
-  return value.replace(DASH_ALPHA_REGEX, (val, letter) => {
-    return letter.toUpperCase();
-  });
+  if (isString(value)) {
+    return value.replace(DASH_ALPHA_REGEX, (val, letter) => {
+      return letter.toUpperCase();
+    });
+  }
+  return value;
 }
