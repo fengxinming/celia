c7e5e6107bbbf0d6289b9cc02e99eed64563ce6b
Jesse Feng <fxm0016@126.com>
2021-04-17 23:46:06 +0800

    Create _isInteger.js

---
+++ src/_isInteger.js
@@ -0,0 +1,8 @@
+import isNumber from './isNumber';
+
+export default function _isInteger(value) {
+  return isNumber(value)
+    && isFinite(value)
+    // eslint-disable-next-line no-bitwise
+    && (value >> 0) === value;
+}
