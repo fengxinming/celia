e0e8a0fa4b9ad094db45dcaeba2bdef2b565077a
Jesse Feng <fxm0016@126.com>
2021-04-18 00:07:32 +0800

    Update isPlainObject.js

--- src/isPlainObject.js
+++ src/isPlainObject.js
@@ -1,5 +1,20 @@
 const { toString } = Object.prototype;
 
-export default function (value) {
+function isObjectLike(value) {
   return !!value && toString.call(value) === '[object Object]';
+}
+
+/**
+ * 判断是否是一个普通对象，兼容ie9+
+ *
+ * @param {any} value
+ * @returns {boolean}
+ */
+export default function isPlainObject(value) {
+  if (!isObjectLike(value)) {
+    return false;
+  }
+
+  const prototype = Object.getPrototypeOf(value);
+  return prototype === null || prototype === Object.prototype;
 }
