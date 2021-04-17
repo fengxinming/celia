f7b7032407e48e0aea3270a97e953c2f82120a43
Jesse Feng <fxm0016@126.com>
2021-04-18 00:05:51 +0800

    Update isLeapYear.js

--- src/isLeapYear.js
+++ src/isLeapYear.js
@@ -1,3 +1,11 @@
-export default function (year) {
+import isNumber from './isNumber';
+import isValidDate from './isValidDate';
+
+export default function isLeapYear(year) {
+  if (isValidDate(year)) {
+    year = year.getFullYear();
+  } else if (!isNumber(year)) {
+    return false;
+  }
   return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
 }
