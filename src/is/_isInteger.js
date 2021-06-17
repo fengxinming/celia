f9b6f906b28d44375cb3fea322d3b36ef26d0cfe
Jesse Feng <fxm0016@126.com>
2021-06-17 10:27:10 +0800

    Delete _isInteger.js

--- src/is/_isInteger.js
+++
@@ -1,8 +0,0 @@
-import isNumber from './isNumber';
-
-export default function _isInteger(value) {
-  return isNumber(value)
-    && isFinite(value)
-    // eslint-disable-next-line no-bitwise
-    && (value >> 0) === value;
-}
